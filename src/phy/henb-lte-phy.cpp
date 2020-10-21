d8067b7b69525e8957654ddc522410887c85d373
RRP-IT <rrp@lx.it.pt>
2020-10-21 19:44:56 +0100

    initializing repository

---
+++ src/phy/henb-lte-phy.cpp
@@ -0,0 +1,32 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2010,2011,2012,2013 TELEMATICS LAB, Politecnico di Bari
+ *
+ * This file is part of LTE-Sim
+ *
+ * LTE-Sim is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation;
+ *
+ * LTE-Sim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LTE-Sim; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author of original file: Francesco Capozzi <f.capozzi@poliba.it>
+ * Author of updated file: Rui R. Paulo <rrp@lx.it.pt>
+ */
+
+#include "henb-lte-phy.h"
+
+HenbLtePhy::HenbLtePhy()
+{
+/* Made by Rui R. Paulo, please cite  doi: 10.1109/VTCFall.2018.8690935 */
+  //SetTxPower(20); //dBm
+}
+
+HenbLtePhy::~HenbLtePhy()
+{}
